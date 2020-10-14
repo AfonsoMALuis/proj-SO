8b41f13ecd46576a170b757757e3f537ebf3a9f8
AfonsoMALuis <afonso.luis@tecnico.ulisboa.pt>
2020-10-14 14:30:21 +0100

    1o commit

---
+++ main.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include "fs/operations.h"
+
+#define MAX_COMMANDS 150000
+#define MAX_INPUT_SIZE 100
+
+int numberThreads = 0;
+
+char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
+int numberCommands = 0;
+int headQueue = 0;
+
+int insertCommand(char* data) {
+    if(numberCommands != MAX_COMMANDS) {
+        strcpy(inputCommands[numberCommands++], data);
+        return 1;
+    }
+    return 0;
+}
+
+char* removeCommand() {
+    if(numberCommands > 0){
+        numberCommands--;
+        return inputCommands[headQueue++];  
+    }
+    return NULL;
+}
+
+void errorParse(){
+    fprintf(stderr, "Error: command invalid\n");
+    exit(EXIT_FAILURE);
+}
+
+void processInput(FILE *inputFile){
+    char line[MAX_INPUT_SIZE];
+
+
+    /* break loop with ^Z or ^D */
+    while (fgets(line, MAX_INPUT_SIZE, inputFile)) {
+        char token, type;
+        char name[MAX_INPUT_SIZE];
+
+        int numTokens = sscanf(line, "%c %s %c", &token, name, &type);
+
+        /* perform minimal validation */
+        if (numTokens < 1) {
+            continue;
+        }
+        switch (token) {
+            case 'c':
+                if(numTokens != 3)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+            
+            case 'l':
+                if(numTokens != 2)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+            
+            case 'd':
+                if(numTokens != 2)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+            
+            case '#':
+                break;
+            
+            default: { /* error */
+                errorParse();
+            }
+        }
+    }
+}
+
+void applyCommands(){
+    while (numberCommands > 0){
+        const char* command = removeCommand();
+        if (command == NULL){
+            continue;
+        }
+
+        char token, type;
+        char name[MAX_INPUT_SIZE];
+        int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+        if (numTokens < 2) {
+            fprintf(stderr, "Error: invalid command in Queue\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int searchResult;
+        switch (token) {
+            case 'c':
+                switch (type) {
+                    case 'f':
+                        printf("Create file: %s\n", name);
+                        create(name, T_FILE);
+                        break;
+                    case 'd':
+                        printf("Create directory: %s\n", name);
+                        create(name, T_DIRECTORY);
+                        break;
+                    default:
+                        fprintf(stderr, "Error: invalid node type\n");
+                        exit(EXIT_FAILURE);
+                }
+                break;
+            case 'l': 
+                searchResult = lookup(name);
+                if (searchResult >= 0)
+                    printf("Search: %s found\n", name);
+                else
+                    printf("Search: %s not found\n", name);
+                break;
+            case 'd':
+                printf("Delete: %s\n", name);
+                delete(name);
+                break;
+            default: { /* error */
+                fprintf(stderr, "Error: command to apply\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 5){
+        printf("A função tem de ter 5 argumentos");
+        return (-1);
+    }
+    struct timeval start, end;
+    /* init filesystem */
+    init_fs();
+
+    /* process input and print tree */
+    FILE *inputFile, *outputFile;
+    inputFile = fopen(argv[1], "r");
+    if(inputFile == NULL) {
+        perror("Error opening file");
+        return(-1);
+    }
+    processInput(inputFile);
+    fclose(inputFile);
+    gettimeofday(&start, NULL);
+    applyCommands();
+    outputFile = fopen(argv[2], "a");
+    print_tecnicofs_tree(outputFile);
+    fclose(outputFile);
+
+    /* release allocated memory */
+    destroy_fs();
+    gettimeofday(&end, NULL);
+    printf("TecnicoFS completed in %.4lf seconds\n",
+           ((end.tv_sec + end.tv_usec * (1e-6))) -
+            (start.tv_sec + start.tv_usec * (1e-6)));
+
+    exit(EXIT_SUCCESS);
+}
