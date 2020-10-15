9b3949b98bdca9c33a3d611a4b13d43ca2063977
AfonsoMALuis <afonso.luis@tecnico.ulisboa.pt>
2020-10-15 17:00:36 +0100

    All Done (?)

--- fs/operations.h
+++ fs/operations.h
@@ -5,9 +5,9 @@
 void init_fs();
 void destroy_fs();
 int is_dir_empty(DirEntry *dirEntries);
-int create(char *name, type nodeType);
-int delete(char *name);
-int lookup(char *name);
+int create(char *name, type nodeType, char *strategy);
+int delete(char *name, char *strategy);
+int lookup(char *name, char *strategy);
 void print_tecnicofs_tree(FILE *fp);
 
 #endif /* FS_H */
