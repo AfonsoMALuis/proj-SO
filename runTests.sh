c47a311a154e48311e64c34686d21e2bdc3d2f21
Ana Sofia Santos <48060313+anasofiiasantos@users.noreply.github.com>
2020-11-18 18:10:45 +0000

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -7,7 +7,7 @@ do
   for i in $(seq 1 ${3})
   do
     input_name="$(basename --suffix=.txt $input)"
-    s
+    echo InputFile= ${input_name} NumThreads= ${i}
     output="$(basename --suffix=.txt $input)"-${i}.txt
     ./tecnicofs $input ${2}/${output} ${i} | grep TecnicoFS
   done
