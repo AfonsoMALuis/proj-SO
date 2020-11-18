4c0575b19a39394acc8c8de72dfa1ee1ffa2194c
Ana Sofia Santos <48060313+anasofiiasantos@users.noreply.github.com>
2020-11-18 17:27:31 +0000

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -2,13 +2,13 @@
 mkdir $2
 
 for input in ${1}/*.txt
-
 do
-
-  echo filename : ==== ${input} ====
+  #echo filename : ==== ${input} ====
   for i in $(seq 1 ${3})
   do
-    echo InputFile= ${input} NumThreads= $i
-    ./tecnicofs $input ${2}/"$(basename --suffix=.txt $input)"-${i}.txt $i
+    input_name="$(basename --suffix=.txt $input)"
+    s
+    output="$(basename --suffix=.txt $input)"-${i}.txt
+    ./tecnicofs $input ${2}/${output} ${i} | grep TecnicoFS
   done
 done
